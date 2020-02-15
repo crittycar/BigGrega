a16999a9c84a2e33254d80f638347c9d976e87b5
august-rg <60901886+august-rg@users.noreply.github.com>
2020-02-15 14:30:23 -0800

    Add files via upload

--- Big_Crime_Code.R
+++ Big_Crime_Code.R
@@ -1,62 +1,80 @@
 #
 # 
-# Big Data Challenge project
+#----------------------Big Data Challenge project-------------------------------
+#
+# To reproduce our findings, run all code below line by line!!
+#
+#--------------any libraries needed are loaded and displayed below--------------
+#
+# none so far
+#
+#--------------make project folders and folder paths----------------------------
+#
+wd <- getwd()  # working directory
 #
 #
-# Libraries below
+folders <- c("Data Output", "Figures", "Data")
+# function to create folders below
+for(i in 1:length(folders)){
+  if(file.exists(folders[i]) == FALSE)
+    dir.create(folders[i])
+}
+#
+# we also need to store the paths to all of these folders
+data.output.path <- paste(wd, "/", folders[1], sep = "")
+figures.path <- paste(wd, "/", folders[2], sep = "")
+data.path <- paste(wd, "/", folders[3], sep = "")
 #
 #
+# now we can save stuff to these folders!
 #
 #
-# lets upload the datas!
-crime <- read.csv("incidents_part1_part2.csv", stringsAsFactors = FALSE)
+#---------------------lets load in the data-------------------------------------
 #
 #
-# we want to ultimately separate the data into separate data frames by year
-# we have some decisions to make. We notice that for the time and date, we have
-# a total of three columns: date, time, and date time
-#
-# we are interested in the daily occurence of crimes, so time is too fine grained
-# for us at this point. We will therefore remove the date-time and time collumns
-# in the interest of speeding up our computations
+# time to upload the datas
+crime <- read.csv(paste(data.path, "/", "incidents_part1_part2.csv", sep = ""),
+                  stringsAsFactors = FALSE)
 #
 #
-# We can use a fairly simple command to do this
+# we now need to simplify our data, there are a lot of columns we do not need
+# we can make a vector below of all the columns we wish to remove
 #
-crime$dispatch_date_time <- crime$dispatch_time <- NULL
-head(crime$dispatch_time)  # we can confirm that the command worked as we hoped
+remove.columns <- c("dispatch_date_time", "dispatch_time", "lat",
+                   "the_geom_webmercator", "dc_key", "ucr_general",
+                   "point_x", "point_y", "dc_dist", "psa", "lng", "the_geom")
+
+# now we want to remove these columns from the data frame.
 #
-# we can also remove other columns that we do not need from this dataset
+for(i in remove.columns){
+crime <- crime[, !(colnames(crime) %in% i)]
+}
+# we can confirm that we still have the columns we want below
+head(crime)
 #
 #
 # now we want to re-format the dispatch_date_time into a better form
-unclass(crime$dispatch_date)  
-# when we use the unclass command, we see that the data is just stored as numbers 
 # we want the years, months, days, and times to be sorted as such so we can access
 # them efficiently and with no potential errors if ex. month and day are the same 
 #
 # so we can make three new columns, one for year, one for month, and one for day
-#
-# need to make this more readable
-crime$dispatch_date <- as.Date(crime$dispatch_date)  # put in a different date format
+
+# we start by putting the dates in a different format
+crime$dispatch_date <- as.Date(crime$dispatch_date)  
 #
 # for ease of use, we can put the date data into separate columns
-# so we now have a year column, a month column, and a day column
+# so we will make a year column, a month column, and a day column
 
-crime$dispatch_year <- (year = as.numeric(format(crime$dispatch_date, format = "%Y")))
-crime$dispatch_month <- (month = as.numeric(format(crime$dispatch_date, format = "%m")))
-crime$dispatch_day <- (day = as.numeric(format(crime$dispatch_date, format = "%d")))
+crime$dispatch_year <- as.numeric(format(crime$dispatch_date, format = "%Y"))
+crime$dispatch_month <- as.numeric(format(crime$dispatch_date, format = "%m"))
+crime$dispatch_day <- as.numeric(format(crime$dispatch_date, format = "%d"))
 
-head(crime)  # to confirm we did what we wanted to do 
-#
-# now we want to take out more data from the dataset to make computations faster
-#
-crime$objectid <- crime$the_geom <- crime$lng <- crime$dc_dist <- crime$psa <- NULL
-crime$lat <- crime$the_geom_webmercator <- crime$dc_key <- crime$ucr_general <- NULL
-crime$point_x <- crime$point_y <- NULL
-#
-# farmer style, we removed all the data that we did not want, about 11 columns
-# 
+# we can confirm that we did what we wanted to 
+head(crime)  
+
+
+# now our data is in a very useable form
+
 # now we can start to ask some questions from our data
 # we want to know if the frequency with which crimes occur on super bowl
 # championship days is different than what we would expect on a normal day
@@ -64,15 +82,16 @@ crime$point_x <- crime$point_y <- NULL
 
 
 # function takes any date in the format YYYY-MM-DD
-# outputs a list of the crime$text_general_code
+# returns a list of the crime$text_general_code AKA frequency of different crimes
 crime.frequency <- function(n){
   freq <- crime$text_general_code[crime$dispatch_date == n]
   return(freq)
 }
 
 
-# now we can easily crime frequencies for any date
 
+# now we can easily determine crime frequencies for any date we want
+# 
 # test to see if it works
 foo <- crime.frequency("2018-02-04")
 table(foo)
@@ -96,23 +115,14 @@ table(please)
 table(test)
 #
 #
-# try to make an empty data frame with 5 columns and nrow(crime) rows
-nrow(crime)
-names <- c("test", "testing", "testtest")
-empties <- rep(NA, length(names))
-cleaned.data <- data.frame(rbind(names, empties))
-?data.frame
-
-
-# we will convert our data into POSIXlt form for ease of use
-crime$dispatch_date_time <- as.POSIXlt(crime$dispatch_date_time)
-
-head(crime$dispatch_date_time)
 
 
 
-years <- crime$dispatch_date[grep("2005", crime$dispatch_date)]
-head(years)
+
+
+
+
+
 #
 #
 # want to first make a function to create an empty vector for every year
