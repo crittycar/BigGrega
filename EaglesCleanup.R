3e7383d1c34ebb52394aec92596fe9ae7011c3be
Jack-Dewey <jack.a.dewey@gmail.com>
2020-02-17 14:19:31 -0800

    Wrote reproducible code for reading multiple csv

--- EaglesCleanup.R
+++ EaglesCleanup.R
@@ -1,21 +1,21 @@
-
+# We need to use the dplyr package for the bind_rows function
+library(dplyr)
 
 # We load in Eagles Football Club data from the timeframe we are examining
 setwd(Eagles)
 eagle.data <- list.files(pattern="*.csv")
 for (i in 1:length(eagle.data)) assign(eagle.data[i], read.csv(eagle.data[i]))
 
+
 # Binding all dataframes into one
-all.eagles <- rbind.data.frame(Eagles2006.csv, Eagles2007.csv, Eagles2008.csv,
-                              Eagles2009.csv, Eagles2010.csv, Eagles2011.csv,
-                              Eagles2012.csv, Eagles2013.csv, Eagles2014.csv,
-                              Eagles2015.csv, Eagles2016.csv, Eagles2017.csv,
-                              Eagles2018.csv, Eagles2019.csv)
+binded.data <- mget(eagle.data)  # mget gets multiple variables 
+all.eagles <- bind_rows(binded.data)  # dpylr allows us to bind data from a list of data frames
 
 
 # Cleaning functions - removing header rows and changing date format.
 cleaned.eagles <- all.eagles[!(all.eagles$X=="Day"),]
 cleaned.dates <- as.Date(cleaned.eagles$X.1, format = "%d-%b-%y")
+
 # Renaming our subset
 date <- cleaned.dates
 # Creating a column of 1s
