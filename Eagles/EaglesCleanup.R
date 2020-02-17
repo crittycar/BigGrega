37f6838a03a9b1974a2f3f401fc91b7e88239f73
Jack-Dewey <60730280+Jack-Dewey@users.noreply.github.com>
2020-02-17 11:29:08 -0800

    Changed pathing of csv

--- Eagles/EaglesCleanup.R
+++ Eagles/EaglesCleanup.R
@@ -1,18 +1,32 @@
 # We load in Eagles Football Club data from the timeframe we are examining
-Eagles2006 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2006.csv")
-Eagles2007 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2007.csv")
-Eagles2008 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2008.csv")
-Eagles2009 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2009.csv")
-Eagles2010 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2010.csv")
-Eagles2011 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2011.csv")
-Eagles2012 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2012.csv")
-Eagles2013 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2013.csv")
-Eagles2014 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2014.csv")
-Eagles2015 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2015.csv")
-Eagles2016 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2016.csv")
-Eagles2017 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2017.csv")
-Eagles2018 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2018.csv")
-Eagles2019 <- read.csv("C:/Users/Jack Dewey/Downloads/Eagles2019.csv")
+Eagles2006 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2006.csv", sep = ""),
+                  stringsAsFactors = FALSE)
+Eagles2007 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2007.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2008 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2008.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2009 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2009.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2010 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2010.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2011 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2011.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2012 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2012.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2013 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2013.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2014 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2014.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2015 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2015.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2016 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2016.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2017 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2017.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2018 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2018.csv", sep = ""),
+                       stringsAsFactors = FALSE)
+Eagles2019 <- read.csv(paste(data.path, "/", "Eagles", "/", "Eagles2019.csv", sep = ""),
+                       stringsAsFactors = FALSE)
 
 # Binding all dataframes into one
 AllEagles <- rbind.data.frame(Eagles2006, Eagles2007, Eagles2008, Eagles2009,
@@ -28,7 +42,7 @@ Date <- AllEagles
 # Creating a column of 1s
 Binary <- c(rep(1,length(AllEagles)))
 # Creating a dataframe with a column of dates and 1s
-EmptyEagles <- data.frame(Date,Binary)
+CompletedEagles <- data.frame(Date,Binary)
 
 
 
