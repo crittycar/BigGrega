cd9ae426d6269211ce86fbb9c8a1ec78c4fd202c
august-rg <60901886+august-rg@users.noreply.github.com>
2020-02-17 16:58:06 -0800

    Add files via upload

--- Big_Crime_Code.R
+++ Big_Crime_Code.R
@@ -1,12 +1,13 @@
 #
 # 
+#
 #----------------------Big Data Challenge project-------------------------------
 #
 # To reproduce our findings, run all code below line by line!!
 #
 #--------------any libraries needed are loaded and displayed below--------------
 #
-# none so far
+library(dplyr)
 #
 #--------------make project folders and folder paths----------------------------
 #
@@ -27,58 +28,48 @@ data.path <- paste(wd, "/", folders[3], sep = "")
 #
 #
 # now we can save stuff to these folders!
-#
-#
-#---------------------lets load in the data-------------------------------------
-#
-#
+
+
+
+#---------------------Below, we upload and clean the philly crime data----------
+
+
 # time to upload the datas
-crime <- read.csv(paste(data.path, "/", "incidents_part1_part2.csv", sep = ""),
+crime <- read.csv(paste(data.path, "/", "Philly_Raw_Crime_Data.csv", sep = ""),
                   stringsAsFactors = FALSE)
+
+
+
 #
 #
 # we now need to simplify our data, there are a lot of columns we do not need
 # we can make a vector below of all the columns we wish to remove
-#
+
 remove.columns <- c("dispatch_date_time", "dispatch_time", "lat",
                    "the_geom_webmercator", "dc_key", "ucr_general",
                    "point_x", "point_y", "dc_dist", "psa", "lng", "the_geom")
 
-# now we want to remove these columns from the data frame.
-#
+# we write a for loop to remove these columns from the data frame
 for(i in remove.columns){
 crime <- crime[, !(colnames(crime) %in% i)]
 }
 # we can confirm that we still have the columns we want below
 head(crime)
-#
-#
-# now we want to re-format the dispatch_date_time into a better form
-# we want the years, months, days, and times to be sorted as such so we can access
-# them efficiently and with no potential errors if ex. month and day are the same 
-#
-# so we can make three new columns, one for year, one for month, and one for day
 
-# we start by putting the dates in a different format
+
+# we also want to put the dates into date format, this will make them easier to 
+# work with later
 crime$dispatch_date <- as.Date(crime$dispatch_date)  
-#
-# for ease of use, we can put the date data into separate columns
-# so we will make a year column, a month column, and a day column
 
-crime$dispatch_year <- as.numeric(format(crime$dispatch_date, format = "%Y"))
-crime$dispatch_month <- as.numeric(format(crime$dispatch_date, format = "%m"))
-crime$dispatch_day <- as.numeric(format(crime$dispatch_date, format = "%d"))
 
-# we can confirm that we did what we wanted to 
-head(crime)  
-?apply
+# our data is now much simpler. We still need to clean some more
+# every single occurence of a crime represents a single observation in our
+# initial data. This is why there are almost three million data point
 
-# now our data is in a very useable form
-
-# now we can start to ask some questions from our data
-# we want to know if the frequency with which crimes occur on super bowl
-# championship days is different than what we would expect on a normal day
-# we can make a function that will give us the frequency of crimes on 
+# we are really interested in the total frequency of crimes on a given day
+# so we want to make a new data frame that has unique dates and the
+# number of crimes that occured on each unique date
+# below we will make a function to help us create the new data frame
 
 
 # function takes any date in the format YYYY-MM-DD
@@ -88,73 +79,104 @@ crime.frequency <- function(n){
   length(freq)
 }
 
-# we now want to create an additional collumn of data that contains 
-# the frequency of all crimes on the selected day
+# we can use this function to create the frequency column in our cleaned data frame
 
 
-
-
-# below we create a new data.frame
-# it turns out that the dates were listed the number of times that crimes occured
-# on that day, and we just want the unique dates
+# below we create a new data.frame 
+# we first have to create an empty one that we can then fill with values
 crime.cleaned <- data.frame(c(rep(NA, length(unique(crime$dispatch_date)))))
 crime.cleaned$unique.date <- sort(unique(crime$dispatch_date))
 
 
 # we want to make a new column in the cleaned data frame
 # we want for this column to contain the frequency of crime for every date
+
+# this vector just contains numbers to help us index
 row.num <- c(1:length(crime.cleaned$unique.date))  
+
+# this vector contains the sorted unique dates
 date.range <- c(crime.cleaned$unique.date[row.num])
+
+# this is the empty column that we will fill with daily crime frequencies
 crime.cleaned$crime_frequency <- rep(NA, length(crime.cleaned$unique.date))
+
+# this loop fills up the above empty vector with daily crime frequencies
 for(i in 1:length(crime.cleaned$crime_frequency))
    crime.cleaned$crime_frequency[i] <- crime.frequency(date.range[i])  
 
+# we remove the NA column that was place-holding
 crime.cleaned$c.rep.NA..length.unique.crime.dispatch_date.... <- NULL
+
+# we can look at our cleaned data to confirm that it looks very nice
 head(crime.cleaned)
 
 
 
-  
-  
+# for analysis, we may want to sort the data by year, or by year and month
+# so we will create three separate columns that contain year, month, day
+crime.cleaned$year <- as.numeric(format(crime.cleaned$unique.date, format = "%Y"))
+crime.cleaned$month <- as.numeric(format(crime.cleaned$unique.date, format = "%m"))
+crime.cleaned$day <- as.numeric(format(crime.cleaned$unique.date, format = "%d"))
 
-# now we can easily determine crime frequencies for any date we want
-
-# function takes three arguments in order
-# YYYY, MM, DD ... note that single digit days cannot have a zero in front
-# returns the average daily frequency of crime in that week
-# by taking three days before, the day of, and three days after
-
-week.average <- function(y, m, d){
-  if(d < 3)  
-    # need to make a different case for every month 28, 29, 30, 31 days long
-    # to do, need to know the number of unique days from previous month
-}
-
-# gonna make a sequence of the days
-# paste them onto year month and then use crime.frequency to calculate the freq
-# then sum all the freq, then need to figure out how to average for each type of crime
-
-sort(unique(crime$dispatch_day[crime$dispatch_month == 12]))  # sorted days of the month
-
-paste("1995", "10", "12", sep = "-")
+# we can look at the data to see that we accomplished what we wanted
+head(crime.cleaned)
 
 
-head(crime$dispatch_month)
-#
-#
-# want to first make a function to create an empty vector for every year
-# then the funciton should store the requested info into the vector
-#
-#
-# write function that sorts data into objects by year
-for (i in c(2006, 2007))
-  vector.name <- i
-  vector.name <- crime$text_general_code[grep(i, crime$dispatch_date)]
-head(vector.name)
-table(vector.name)
-qa#
-#
-# crime$text_general_code will give the name of the crime
-test <- crime$text_general_code[grep("2006", crime$dispatch_date)]
-head(test)
-unclass(head(crime$dispatch_date))
+
+#--------------now we want to load in and clean the eagles data----------------- 
+
+# this is where we use the dplyr library we loaded at the beginning
+# for the bind_rows function
+
+# We load in Eagles Football Club data from the timeframe we are examining
+
+# set the working directory to the file containing the Eagles data
+# which is located in our primary R file
+setwd("Eagles")
+
+# The list.files function identifies all .csv files in the Eagles folder
+eagle.data <- list.files(pattern="*.csv")
+for (i in 1:length(eagle.data)) assign(eagle.data[i], read.csv(eagle.data[i]))
+
+
+# Binding all dataframes into one
+binded.data <- mget(eagle.data)  # mget gets multiple variables 
+all.eagles <- bind_rows(binded.data)  # dpylr allows us to bind data from a list of data frames
+
+
+# Cleaning functions - removing header rows and changing date format.
+cleaned.eagles <- all.eagles[!(all.eagles$X.1=="Day"),]
+cleaned.dates <- as.Date(cleaned.eagles$X.2, format = "%d-%b-%y")
+
+# Renaming our subset
+unique.date <- cleaned.dates
+# Creating a column of 1s
+binary <- c(rep(1,length(cleaned.dates)))
+# Creating a dataframe with a column of dates and 1s
+completed.eagles <- data.frame(unique.date,binary)
+
+
+first(completed.eagles$unique.date)
+last(completed.eagles$unique.date)
+
+#------------------------combining our two datasets-----------------------------
+
+# we now want to merge our two cleaned data sets into a single data.frame
+# recall that the cleaned datas are stored in 
+# completed.eagles and crime.cleaned
+
+# we will use the merge function to merge by date and combine them into one dataset
+combined.data <- merge(crime.cleaned, completed.eagles, all.x = TRUE)
+
+# the only problem now is that we have NA values where there is not a game
+# we want for there to be a zero instead of an NA value
+
+# we can use the function below to change all the NA's to 0
+combined.data[is.na(combined.data)] <- 0
+
+
+
+
+
+
+
