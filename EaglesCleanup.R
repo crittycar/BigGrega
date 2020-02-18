d659c9fe73b7a19898f03b81e3a54407e6014ecc
Jack-Dewey <jack.a.dewey@gmail.com>
2020-02-17 16:15:35 -0800

    Adding more comments

--- EaglesCleanup.R
+++ EaglesCleanup.R
@@ -2,6 +2,10 @@
 library(dplyr)
 
 # We load in Eagles Football Club data from the timeframe we are examining
+# The object 'Eagles' is assigned and run from our primary R file, BigDataProject.R
+# The 'list.files' function identifies all ".csv" files in our working directory
+# The loop then reads each sequential csv file from 2006 to 2019 and applies the
+# file name as their object name
 setwd(Eagles)
 eagle.data <- list.files(pattern="*.csv")
 for (i in 1:length(eagle.data)) assign(eagle.data[i], read.csv(eagle.data[i]))
@@ -14,7 +18,7 @@ all.eagles <- bind_rows(binded.data)  # dpylr allows us to bind data from a list
 
 # Cleaning functions - removing header rows and changing date format.
 cleaned.eagles <- all.eagles[!(all.eagles$X=="Day"),]
-cleaned.dates <- as.Date(cleaned.eagles$X.1, format = "%d-%b-%y")
+cleaned.dates <- as.Date(cleaned.eagles$X.2, format = "%d-%b-%y")
 
 # Renaming our subset
 date <- cleaned.dates
