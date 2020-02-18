1c01d77fb809eba6fd1681fa48c7fda5779076ae
crittycar <60585941+crittycar@users.noreply.github.com>
2020-02-18 00:49:40 -0800

    fixed 06 year end

--- PracticePlots.R
+++ PracticePlots.R
@@ -1,42 +1,100 @@
-#Name: Michael Lee
-#Date: 13/02/20
-#Title: Big Data Project
-#
-#Description: We look at the crimes committed in Philadelphia between 2006 and 
-#2020 and compare crime rates between times when the Philadelphia Eagles played 
-#games and times when they did not.
-#
-#Index: 
-# 1. Installation
-# 2. Subsets
-# 3. Functions and Tests
+#Analysis
 
-#--------------------------------- 1. Installation -----------------------------
-
-#******************** Working Path and Directory Creation **********************
-
-#Working directory subfolders pathways
-PhillyCrimeRates <- paste(getwd(),"/","Philly Crime Rates","/",sep ="")
-Eagles <- paste(getwd(),"/","Eagles","/",sep ="")
-Analysis <- paste(getwd(),"/","Analysis","/",sep ="")
+getwd()
 
 
-#******************* Installing the CSV files **********************************
+combo <- read.csv("C:/Users/user/Downloads/combined.data.csv")
+View(combo)
 
-#This reads the philly crime rates csv file
-phillycrime <- read.csv(paste(PhillyCrimeRates, 
-                              "incidents_part1_part2", 
-                              sep = ""))
+as.Date(combo$unique.date)
 
-#This reads the eagles playoff games csv file
-Eagles_Playoffs <- read.csv(paste(Eagles,
-                                  "EaglesPlayoffs.csv",
-                                  sep = ""))
+View(combo)
+data.frame(combo)
+
+combo[combo$unique.date >= "9-10-2006" & combo$unique.date <= "1-7-2007",]
+
+install.packages("selectByDate")
+library(selectByDate)
+
+data.1999 <- selectByDate(combo$unique.date, start = "9-10-2006", end = "1-7-2007")
+head(data.1999)
+
+boxplot(combo$crime_frequency~combo$binary)
+
+
+erif <- hist(combo$crime_frequency)
+plot(erif)
 
 
 
-#--------------------------------- 2. Subsets ----------------------------------
+#y=game days
+y= combo$crime_frequency[combo$binary == 1]
+length(y)
 
+#non game days
+x= combo$crime_frequency[combo$binary == 0]
+length(x)
+t.test(x,y)
+
+
+t.test(x,y, alternative = "two.sided")
+
+
+?oneway.test(x~y)
+
+#Log Regression
+
+#Logistic Regression Code
+
+x <- combo$crime_frequency
+y <- combo$binary
+#All six fixed effects models
+# Logistic regression
+z <- glm(y ~ x,family = binomial(link="logit"), data = combo)
+
+#install.packages("visreg")
+library(visreg)
+# Visualize fit on the original scale
+visreg(z, xvar = "x", ylim = range(y), rug = 2, scale = "response") 
+
+# Visualize fit on the transformed scale (showing working values)
+visreg(z, xvar = "x")  
+
+plot(jitter(y, amount = 0.02) ~ x, data = mydata)
+yhat <- fitted(z)
+lines(yhat[order(x)] ~ x[order(x)])
+
+summary(z)
+
+library(MASS)
+confint(z, level = 0.95)
+
+library(MASS)
+confint(z, level = 0.95) # approximate 95% confidence intervals
+
+anova(z, test = "Chisq")
+ 
+
+y= combo$crime_frequency[combo$binary == 1]
+
+#non game days
+x= combo$crime_frequency[combo$binary == 0]
+
+
+obj1 <- c(1:100)
+obj2 <- c(1:100)
+obj3 <- c(1:1000)
+
+objects <- c("obj1","obj2","obj3")
+
+for (i in 1:length(objects)) {
+  jpeg(paste(objects[i], ".jpg", sep=""))
+  boxplot(get(objects[i]))
+  dev.off()
+}
+
+
+#T-tests for each season 2006-2019
 #We assign the values in unique.date as actual dates
 dateframe <- as.Date(combined.data$unique.date)
 
@@ -44,22 +102,17 @@ dateframe <- as.Date(combined.data$unique.date)
 googy <- cbind(combined.data, dateframe )
 
 #This is the line to create a subset of the dates entered
-googy[googy$dateframe >= "2006-01-01" & googy$dateframe <= "2006-02-02",]
-
-
-
-#--------------------------------- 3. Functions and Tests ----------------------
 
 ###2006 Season
-Season2006<- googy[googy$dateframe >= "2006-09-10" & googy$dateframe <= "2007-01-13",]
+Season2006<- googy[googy$dateframe >= "2006-09-10" & googy$dateframe <= "2007-01-07",]
 
 y2006= Season2006$crime_frequency[Season2006$binary == 1]
-length(x2006)
+
 #non game days
 x2006= Season2006$crime_frequency[Season2006$binary == 0]
 
-p06 <- t.test(x2006,y2006)
-p06
+t.test(x2006,y2006)
+
 ###2007
 Season2007<- googy[googy$dateframe >= "2007-09-09" & googy$dateframe <= "2007-12-30",]
 
@@ -68,7 +121,7 @@ y2007= Season2007$crime_frequency[Season2007$binary == 1]
 #non game days
 x2007= Season2007$crime_frequency[Season2007$binary == 0]
 
-p07 <- t.test(x2007,y2007)
+t.test(x2007,y2007)
 
 ###2008
 Season2008<- googy[googy$dateframe >= "2008-09-07" & googy$dateframe <= "2009-01-18",]
@@ -78,7 +131,7 @@ y2008= Season2008$crime_frequency[Season2008$binary == 1]
 #non game days
 x2008= Season2008$crime_frequency[Season2008$binary == 0]
 
-p08 <- t.test(x2008,y2008)
+t.test(x2008,y2008)
 
 ###2009
 Season2009<- googy[googy$dateframe >= "2009-09-13" & googy$dateframe <= "2010-01-09",]
@@ -88,7 +141,7 @@ y2009= Season2009$crime_frequency[Season2009$binary == 1]
 #non game days
 x2009= Season2009$crime_frequency[Season2009$binary == 0]
 
-p09 <- t.test(x2009,y2009)
+t.test(x2009,y2009)
 
 ###2010
 Season2010<- googy[googy$dateframe >= "2010-09-12" & googy$dateframe <= "2011-01-09",]
@@ -98,7 +151,7 @@ y2010= Season2010$crime_frequency[Season2010$binary == 1]
 #non game days
 x2010= Season2010$crime_frequency[Season2010$binary == 0]
 
-p10 <- t.test(x2010,y2010)
+t.test(x2010,y2010)
 
 ###2011
 Season2011<- googy[googy$dateframe >= "2011-09-11" & googy$dateframe <= "2012-01-01",]
@@ -108,7 +161,7 @@ y2011= Season2011$crime_frequency[Season2011$binary == 1]
 #non game days
 x2011= Season2011$crime_frequency[Season2011$binary == 0]
 
-p11 <- t.test(x2011,y2011)
+t.test(x2011,y2011)
 
 ###2012
 Season2012<- googy[googy$dateframe >= "2012-09-09" & googy$dateframe <= "2012-12-30",]
@@ -118,7 +171,7 @@ y2012= Season2012$crime_frequency[Season2012$binary == 1]
 #non game days
 x2012= Season2012$crime_frequency[Season2012$binary == 0]
 
-p12 <- t.test(x2012,y2012)
+t.test(x2012,y2012)
 
 ###2013
 Season2013<- googy[googy$dateframe >= "2013-09-09" & googy$dateframe <= "2014-01-04",]
@@ -128,7 +181,7 @@ y2013= Season2013$crime_frequency[Season2013$binary == 1]
 #non game days
 x2013= Season2013$crime_frequency[Season2013$binary == 0]
 
-p13 <- t.test(x2013,y2013)
+t.test(x2013,y2013)
 
 ###2014
 Season2014<- googy[googy$dateframe >= "2014-09-07" & googy$dateframe <= "2014-12-28",]
@@ -138,7 +191,7 @@ y2014= Season2014$crime_frequency[Season2014$binary == 1]
 #non game days
 x2014= Season2014$crime_frequency[Season2014$binary == 0]
 
-p14 <- t.test(x2014,y2014)
+t.test(x2014,y2014)
 
 ###2015
 Season2015<- googy[googy$dateframe >= "2015-09-14" & googy$dateframe <= "2016-01-03",]
@@ -148,7 +201,7 @@ y2015= Season2015$crime_frequency[Season2015$binary == 1]
 #non game days
 x2015= Season2015$crime_frequency[Season2015$binary == 0]
 
-p15 <- t.test(x2015,y2015)
+t.test(x2015,y2015)
 
 ###2016
 Season2016<- googy[googy$dateframe >= "2016-09-11" & googy$dateframe <= "2017-01-01",]
@@ -158,7 +211,7 @@ y2016= Season2016$crime_frequency[Season2016$binary == 1]
 #non game days
 x2016= Season2016$crime_frequency[Season2016$binary == 0]
 
-p16 <- t.test(x2016,y2016)
+t.test(x2016,y2016)
 
 ###2017
 Season2017<- googy[googy$dateframe >= "2017-09-10" & googy$dateframe <= "2018-02-04",]
@@ -168,7 +221,7 @@ y2017= Season2017$crime_frequency[Season2017$binary == 1]
 #non game days
 x2017= Season2017$crime_frequency[Season2017$binary == 0]
 
-p17 <- t.test(x2017,y2017)
+t.test(x2017,y2017)
 
 ###2018
 Season2018<- googy[googy$dateframe >= "2018-09-06" & googy$dateframe <= "2019-01-13",]
@@ -178,7 +231,7 @@ y2018= Season2018$crime_frequency[Season2018$binary == 1]
 #non game days
 x2018= Season2018$crime_frequency[Season2018$binary == 0]
 
-p18 <-t.test(x2018,y2018)
+t.test(x2018,y2018)
 
 ###2019
 Season2019<- googy[googy$dateframe >= "2019-09-08" & googy$dateframe <= "2020-01-05",]
@@ -188,8 +241,4 @@ y2019= Season2019$crime_frequency[Season2019$binary == 1]
 #non game days
 x2019= Season2019$crime_frequency[Season2019$binary == 0]
 
-p19 <- t.test(x2019,y2019)
-
-#compile all results
-
-T.test.results <- capture.output(p06,p07,p08,p09,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19, file = "Results.csv")
+t.test(x2019,y2019)
